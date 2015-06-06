41cd11f66b1d0d4950816d97c02b78c9d95ba924
cory pisano <cory.pisano@gmail.com>
2015-06-06 15:02:42 -0400

    hw2

---
+++ module2.m
@@ -0,0 +1,34 @@
+%% Cory Pisano - Homework 2
+
+%% Problem 3
+
+% (a)
+
+x = [1 2 3 4 0 0 0 0];   n = 0:7;   h = [1 1 1 1 0 0 0 0];
+y = conv(x, h)
+stem(y)
+
+% (b)
+figure()
+x = [1 2 -1 0 0 0];   n = 0:5;   h = [1 -1 1 -1 0 0];
+y = conv(x,h)
+stem(y)
+
+% (c)
+figure()
+x = [1 2 -1 0 0 0];   n = 0:5;   h = x;
+y = conv(x,h)
+stem(y)
+
+% (d)
+figure()
+x = [1 1 2 0 0];   n = 0:4;   h = [1 1 1 1 1];
+y = conv(x, h)
+stem(y)
+
+%% Problem 4
+
+%% Problem 5
+
+%% Problem 6
+
